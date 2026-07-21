1e2049b8cbea3093bf6cc3c8971c8e47abd95b2b
Tanishq Bhatia <tanishq@example.com>
2026-07-21 21:32:44 +0530

    Add sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,6 +30,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
+        <SpeedInsights />
+        <Analytics/>
     </html>
   );
 }
