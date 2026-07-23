b974b7602df53958032eabdd689de7c065b8b35d
Tanishq Bhatia <tanishq@example.com>
2026-07-23 22:09:36 +0530

    Added Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,8 +32,12 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+
         <SpeedInsights />
         <Analytics />
+
+        {/* Google Analytics */}
+        <GoogleAnalytics gaId="G-RLVKPSSBF1" />
       </body>
     </html>
   );
